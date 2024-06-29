057ece8008fafff3caad1524296b2b0224ae4ce4
Davi Nascimento <davileite2007@gmail.com>
2024-06-29 04:42:32 -0300

    Added score, runtime, genres and trailers (in progress)

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,21 @@
 $(()=>{
-
   var menuOpen = false;
   var menuAnimation = false;
   var scrollingUp = false;
 
   movieRatings = ['\
+    <i class="fa-regular fa-star"></i>\
+    <i class="fa-regular fa-star"></i>\
+    <i class="fa-regular fa-star"></i>\
+    <i class="fa-regular fa-star"></i>\
+    <i class="fa-regular fa-star"></i>','\
+    \
+    <i class="fa-solid fa-star-half-stroke"></i>\
+    <i class="fa-regular fa-star"></i>\
+    <i class="fa-regular fa-star"></i>\
+    <i class="fa-regular fa-star"></i>\
+    <i class="fa-regular fa-star"></i>','\
+    \
     <i class="fa-solid fa-star"></i>\
     <i class="fa-regular fa-star"></i>\
     <i class="fa-regular fa-star"></i>\
@@ -60,8 +71,96 @@ $(()=>{
     <i class="fa-solid fa-star"></i>'
   ]
 
+  function generateRating(score){
+    if(score == 'Score not yet available')
+      return '<span style="color: white">Score not available</span>'
+
+    for(let i = 1; i < movieRatings.length + 1; i++){
+      if(score <= i * 10 && score != 100){
+        return movieRatings[i - 1]
+      }
+    }
+
+    return movieRatings[10]
+  }
+
+  function setColor(score) {
+    let value = Math.min(Math.max(score, 0), 100);
+    let greenAmount = Math.round((value / 100) * 255);
+    let redAmount = 255 - greenAmount;
+    let color = `rgb(${redAmount}, ${greenAmount}, 0)`;
+
+    return color
+  }
+
+  function setRatingBox(score, views, target){
+    if(score != 'Score not yet available'){
+      score = Number(score)
+
+      if(target){
+        return '\
+        <div class="movie-rating">\
+          ' + '<span class="movie-score" style="background-color: ' + setColor(score) + '">' + score + '</span>' + generateRating(score) + '\
+        </div>'
+
+      }else{
+        return '\
+        <div class="description-rating">\
+          ' + '<span class="movie-score" style="background-color: ' + setColor(score) + '; color: black">' + score + '</span>' + generateRating(score) + '<span><i class="fa-solid fa-eye"></i>' + views + '</span>\
+        </div>'
+      }
+
+    }else{
+      if(target){
+        return '\
+        <div class="movie-rating">\
+          <span>Score not available</span>\
+        </div>'
+
+      }else{
+        return '\
+        <div class="description-rating">\
+          <span>Score not available</span>' + '<span><i class="fa-solid fa-eye"></i>' + views + '</span>\
+        </div>'
+      }
+    }
+  }
+
   function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function numberConvert(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  function generateViews(score){
+    let number;
+
+    if(score != 'Score not yet available'){
+      score = Number(score)
+      let equation = score ** (3.4 + (score/(-1500)))
+      let min = equation;
+      let max = equation * 1.3;
+
+      if(score == 0)
+        score = 1
+
+      number = randomNumber(min, max)
+
+    }else{
+      number = randomNumber(1000, 10000)
+    }
+
+    let views = Math.floor(number);
+    return numberConvert(views);
+  }
+
+  function generateTopTag(score){
+    if(Number(score) >= 90)
+      return '<div class="top-tag"><p>TOP MOVIE ✨</p></div>'
+    else
+      return ''
   }
 
   function shuffleArray(arr) {
@@ -71,123 +170,123 @@ $(()=>{
     }
   }
 
-  function generateNewTag(){
-    let newTag;
-    let dice = randomNumber(1, 10);
-    
-    if(dice == 1)
-      newTag = '<div class="new-tag"><p>NEW!</p></div>'
-    else
-      newTag = ''
-
-      return newTag;
-  }
   var catalogue = baseCatalogue;
   shuffleArray(catalogue);
   catalogue.unshift({})
 
   for(let i = 1; i < catalogue.length; i++){
-    let rating = movieRatings[Math.floor(Math.random() * movieRatings.length)];
-    let views = randomNumber(10000, 800000);
+    let rating = generateRating(catalogue[i].score);
+    let views = generateViews(catalogue[i].score);
+    let topTag = generateTopTag(catalogue[i].score);
 
     if(i % 28 != 0){
-      let newTag = generateNewTag();
-
       $('.main').append('\
-      <div class="movie-wrapper">\
-        <div class="movie" id="movie-' + i + '">\
-          <div class="image" title=\'' + 'WATCH "' + catalogue[i].title.toUpperCase() + '" ON YOUR FAVORITE STREAMING SERVICE RIGHT NOW!\'>\
-            <img src="' + catalogue[i].image + '">'
-            + newTag +
-            '<div class="image-shadow"></div>\
-          </div>\
-          <div class="movie-info">\
-            <div class="movie-rating">\
-              ' + rating + '\
+        <div class="movie-wrapper">\
+          <div class="movie" id="movie-' + i + '" video-frame="' + i + '">\
+            <div class="image" title=\'' + 'WATCH "' + catalogue[i].title.toUpperCase() + '" ON YOUR FAVORITE STREAMING SERVICE RIGHT NOW!\'>\
+              <img src="' + catalogue[i].image + '">'
+              + topTag +
+              '<div class="image-shadow"></div>\
             </div>\
-            <div class="title">\
-              <h3>' + catalogue[i].title + '</h3>\
+            <div class="movie-info">\
+              <div class="movie-rating">\
+                ' + rating + '\
+              </div>\
+              <div class="title">\
+                <h3>' + catalogue[i].title + '</h3>\
+              </div>\
             </div>\
           </div>\
         </div>\
-      </div>');
+      ');
 
       $('.descriptions').append('\
-          <div class="description" id="movie-' + i + '">\
-            <div class="description-header">\
-              <h3>' + catalogue[i].title + ' <span id="movie-' + i + '">X</span><div class="clear"></div></h3>\
-            </div>\
-            <div class="description-body">\
-              <div class="description-rating">\
-                ' + rating + '<span><i class="fa-solid fa-eye"></i>' + views + '</span>\
-              </div>\
-              <p>' + catalogue[i].description + '</p>\
-            </div>\
-            <div class="description-footer">\
-              <a href="" class="watch-stream">\
-                <i class="fa-solid fa-circle-play"></i>Watch on streaming\
-              </a>\
-              <a href="" class="add-library">\
-                <i class="fa-regular fa-bookmark"></i>Add to my list\
-              </a>\
-              <a href="" class="share-movie">\
-                <i class="fa-solid fa-share"></i>Share this movie\
-              </a>\
-            </div>\
+        <div class="description" id="movie-' + i + '">\
+          <div class="description-header">\
+            <h3>' + catalogue[i].title + '</h3><span class="movie-release">Release Date: ' + catalogue[i].release + '</span><span class="description-close-button" id="movie-' + i + '">X</span>\
           </div>\
-        ')
+          <div class="iframe-wrapper"></div>\
+          <div class="description-body">'
+            + setRatingBox(catalogue[i].score, views, false) +
+            '<div class="movie-details">\
+              <p>Runtime: ' + catalogue[i].runtime + '</p>\
+              <p>Genre: ' + catalogue[i].genres + '</p>\
+            </div>\
+            <p>' + catalogue[i].description + '</p>\
+          </div>\
+          <div class="description-footer">\
+            <a href="" class="watch-stream">\
+              <i class="fa-solid fa-circle-play"></i>Watch on streaming\
+            </a>\
+            <a href="" class="add-library">\
+              <i class="fa-regular fa-bookmark"></i>Add to watchlist\
+            </a>\
+            <a href="" class="share-movie">\
+              <i class="fa-solid fa-share"></i>Share this movie\
+            </a>\
+          </div>\
+        </div>\
+      ')
 
     }else{
       $('.main').append('\
-      <div class="movie-banner-wrapper">\
-        <div class="movie-banner">\
-          <div class="image">\
-            <img src="' + catalogue[i].image + '">\
-            <div class="image-shadow"></div>\
+        <div class="movie-banner-wrapper">\
+          <div class="movie-banner">\
+            <div class="image" id="movie-' + i + '">\
+              <img src="' + catalogue[i].image + '">\
+              <div class="image-shadow"></div>'
+              + topTag +
+            '</div>\
+            <div class="movie-banner-info">\
+              <div class="movie-banner-header">\
+                <h2><i class="fa-solid fa-arrow-up-right-dots"></i>FEATURING THIS MONTH! <span><i class="fa-solid fa-eye"></i>' + views + '</span><div class="clear"></div></h2>\
+              </div>\
+              <div class="movie-banner-body">\
+                <div class="movie-banner-image">\
+                  <img src="' + catalogue[i].image + '">\
+                  <div class="movie-banner-image-shadow"></div>'
+                  + topTag +
+                '</div>'
+                + setRatingBox(catalogue[i].score, views, true) +
+                '<div class="title">\
+                  <h2>' + catalogue[i].title + '</h2>\
+                </div>\
+                <div class="movie-details">\
+                  <p>Release Date: ' + catalogue[i].release + '</p>\
+                  <p>Runtime: ' + catalogue[i].runtime + '</p>\
+                  <p>Genre: ' + catalogue[i].genres + '</p>\
+                </div>\
+                <div class="description">\
+                  <p>' + catalogue[i].description + '</p>\
+                </div>\
+              </div>\
+              <div class="movie-banner-footer">\
+                <h2>\
+                  <a href="" class="watch-stream">\
+                    <i class="fa-solid fa-circle-play"></i>WATCH ON STREAM\
+                  </a>\
+                  <a href="" class="add-library">\
+                    <i class="fa-regular fa-bookmark"></i>ADD TO WATCHLIST\
+                  </a>\
+                  <a href="" class="share-movie">\
+                    <i class="fa-solid fa-share"></i>SHARE THIS MOVIE\
+                  </a>\
+                </h2>\
+              </div>\
+            </div>\
+            <div class="clear"></div>\
           </div>\
-          <div class="movie-banner-info">\
-            <div class="movie-banner-header">\
-              <h2><i class="fa-solid fa-arrow-up-right-dots"></i>FEATURING THIS MONTH! <span><i class="fa-solid fa-eye"></i>' + views + '</span><div class="clear"></div></h2>\
-            </div>\
-            <div class="movie-banner-body">\
-              <div class="movie-banner-image">\
-                <img src="' + catalogue[i].image + '">\
-                <div class="movie-banner-image-shadow"></div>\
-              </div>\
-              <div class="movie-rating">\
-              ' + rating + '\
-              </div>\
-              <div class="title">\
-                <h2>' + catalogue[i].title + '</h2>\
-              </div>\
-              <div class="description">\
-                <p>' + catalogue[i].description + '</p>\
-              </div>\
-            </div>\
-            <div class="movie-banner-footer">\
-              <h2>\
-                <a href="" class="watch-stream">\
-                  <i class="fa-solid fa-circle-play"></i>WATCH ON STREAM\
-                </a>\
-                <a href="" class="add-library">\
-                  <i class="fa-regular fa-bookmark"></i>ADD TO MY LIST\
-                </a>\
-                <a href="" class="share-movie">\
-                  <i class="fa-solid fa-share"></i>SHARE THIS MOVIE\
-                </a>\
-              </h2>\
-            </div>\
-          </div>\
-          <div class="clear"></div>\
         </div>\
-      </div>')
+      ')
     }
   }
 
   $('.movie').click(function(){
     let id = $(this).attr('id');
     let element = $('.descriptions #' + id);
+    let index = $(this).attr('video-frame')
 
+    $(element).find('.iframe-wrapper').append(catalogue[Number(index)].iframe);
     $('.descriptions').fadeIn(250);
     $(element).fadeIn(250);
     $('body').css('overflow', 'hidden');
@@ -199,7 +298,8 @@ $(()=>{
 
     $('.descriptions').fadeOut(250);
     $(element).fadeOut(250);
-    $('body').css('overflow', 'auto')
+    $('body').css('overflow', 'auto');
+    setTimeout(()=>{$(element).find('.iframe-wrapper').empty()}, 250);
   })
 
   $('.menu-bar').click(function(){
--- scraping.py
+++ scraping.py
@@ -1,35 +1,118 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pyperclip
 import json
 
-url = 'https://www.rottentomatoes.com/browse/movies_at_home/sort:popular?page=5'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
 
+def getSoup(url):
+  response = requests.get(url)
+  return BeautifulSoup(response.content, 'html.parser')
+
+
+def ddGet(element):
+  dt = element.find_parent('dt')
+  dd = dt.find_next('dd')
+  return dd
+
+
+def getSel(url):
+  driver = webdriver.Chrome()
+  return driver.get(url)
+
+
+def waitElement(xpath):
+  global wait
+  return wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+
+
+soup = getSoup('https://www.rottentomatoes.com/browse/movies_at_home/sort:popular?page=8')
 frames = soup.select(".discovery-tiles__wrap div")
 
 catalogue = []
+movies = 0
 
 for i, frame in enumerate(frames):
   if(i % 2 == 0):
+    movies += 1
+
+    # Get Image
     image = frame.select('rt-img')
     imageLink = image[0].get('src')
 
+    # Get Title
     title = image[0].get('alt')
 
+    # Get Description
     link = frame.select('a')
     href = link[0].get('href')
-    externalLink = 'https://www.rottentomatoes.com/' + href
-    soup2 = BeautifulSoup(requests.get(externalLink).content, 'html.parser')
+    soup2 = getSoup('https://www.rottentomatoes.com/' + href)
     descriptionSlot = soup2.find_all(attrs={'slot': 'description'})
-    content = descriptionSlot[0].find_all(attrs={'slot': 'content'})
-    descricao = content[0].get_text().strip()
+    contentSlot = descriptionSlot[0].find_all(attrs={'slot': 'content'})
+    description = contentSlot[0].get_text().strip()
+
+    # Get Genres
+    genreString = soup2.find(string='Genre')
+    rtList = ddGet(genreString).find_all('rt-link')
+    genreList = []
+    for rt in rtList:
+      genre = rt.get_text()
+      genreList.append(genre)
+    genres = ', '.join(genreList)
+    
+    # Get Release Date
+    dateElement = ''
+    dateTheaters = soup2.find(string='Release Date (Theaters)')
+    if dateTheaters == None:
+      dateStreaming = soup2.find(string='Release Date (Streaming)')
+      dateElement = dateStreaming
+    else:
+      dateElement = dateTheaters
+    rt = ddGet(dateElement).find('rt-text')
+    date = rt.get_text()
+    dateStrings = date.split(', ')
+    releaseDate = f'{dateStrings[0]}, {dateStrings[1]}'
+
+    # Get Runtime
+    runtimeString = soup2.find(string='Runtime')
+    rt = ddGet(runtimeString).find('rt-text')
+    runtime = rt.get_text()
+
+    # Get Score
+    audienceSlot = soup2.find_all(attrs={'slot': 'audienceScore'})
+    rt = audienceSlot[0].find_all('rt-text')
+    score = rt[0].get_text()[:-1]
+    if score == '':
+      score = 'Score not yet available'
+    
+    # Get Trailer
+    titleSplit = title.split()
+    linkTitle = '+'.join(titleSplit)
+    driver = webdriver.Chrome()
+    driver.get('https://www.youtube.com/results?search_query=' + linkTitle + '+english+trailer')
+    wait = WebDriverWait(driver, 5)
+    waitElement('//*[@id="video-title"]/yt-formatted-string').click()
+    waitElement('//*[@id="top-level-buttons-computed"]/yt-button-view-model/button-view-model/button').click()
+    waitElement('//*[@id="target"]').click()
+    waitElement('//*[@id="action-buttons"]/yt-button-renderer/yt-button-shape/button').click()
+    driver.quit()
+    iframe = pyperclip.paste()
+
 
     catalogue.append({
     'image': imageLink,
     'title': title,
-    'description': descricao
+    'description': description,
+    'genres': genres,
+    'release': releaseDate,
+    'runtime': runtime,
+    'score': score,
+    'iframe': iframe
   })
+
 
 with open('movies.json', 'w') as f:
   json.dump(catalogue, f)
